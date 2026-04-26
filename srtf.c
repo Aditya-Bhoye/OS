7cf323b322ce22f4a518bddd3cd90933ac032fc9
Aditya Bhoye <adibhoye@gmail.com>
2026-04-26 06:36:54 +0530

    Refactor SRTF implementation for clarity and efficiency

--- srtf.c
+++ srtf.c
@@ -1,47 +1,39 @@
-#include<stdio.h>
+#include <stdio.h>
 int main(){
-    int n,i,j,temp;
-    int at[20],bt[20],ct[20],tat[20],wt[20],rem[20],done[20],order[i];
-    printf("Enter number of process you want : ");
+    int n,i;
+    int at[20],bt[20],ct[20],tat[20],wt[20],rem[20],exec[100];
     scanf("%d",&n);
     for(i=0;i<n;i++){
-        printf("Enter arrival time for process P%d : ",i);
-        scanf("%d",&at[i]);
-        printf("Enter brust time for process P%d : ",i);
-        scanf("%d",&bt[i]);
-        order[i]=i;
-        done[i]=0;
+        scanf("%d%d",&at[i],&bt[i]);
         rem[i]=bt[i];
     }
-    int time=0,completed=0;
+    int time=0,completed=0,k=0;
     while(completed<n){
-        int min_rem=9999,idx=-1;
+        int idx=-1;
         for(i=0;i<n;i++){
-            if(!done[i] && at[i]<=time && bt[i]<min_rem){
-                min_rem=rem[i];
-                idx=i;
+            if(rem[i]>0 && at[i]<=time){
+                if(idx==-1 || rem[i]<rem[idx] || (rem[i]==rem[idx] && at[i]<at[idx]))
+                    idx=i;
             }
         }
-        if(idx==-1){ time++ ; continue ;}
+        if(idx==-1){ exec[k++]=-1; time++; continue; }
+        exec[k++]=idx;
         rem[idx]--;
         time++;
         if(rem[idx]==0){
             ct[idx]=time;
-            done[idx]=1;
             completed++;
         }
     }
-    float total_tat=0,total_wt=0;
     for(i=0;i<n;i++){
         tat[i]=ct[i]-at[i];
         wt[i]=tat[i]-bt[i];
-        total_wt+=wt[i];
-        total_tat+=tat[i];
     }
-    printf("\nProcess\tAT\tBT\tCT\tTAT\tWT\n");
-    for(i=0;i<n;i++){
-        printf("\nP%d\t%d\t%d\t%d\t%d\t%d",order[i],at[i],bt[i],ct[i],tat[i],wt[i]);
+    for(i=0;i<k;i++){
+        if(exec[i]==-1) printf("idle ");
+        else printf("P%d ",exec[i]);
     }
-    printf("\nAVG TAT =%.2f",total_tat/n);
-    printf("\nAVG WT =%.2f\n",total_wt/n);
+    printf("\n");
+    for(i=0;i<n;i++)
+        printf("P%d %d %d %d %d %d\n",i,at[i],bt[i],ct[i],tat[i],wt[i]);
 }
